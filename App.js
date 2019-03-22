d199e7651a077a43960effeffc9b258b1e922bcf
Steve Mu <qmu@bu.edu>
2019-03-22 11:04:14 -0400

    backup - move content with animation working

--- App.js
+++ App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Animated } from 'react-native';
 import { MapView } from 'expo';
 
+
+// The map will stay at the same size
+// the content is a layer above the map
+// the map is below the content via zIndex
+// when content is moved up and down, the map top margin is adjusted accordingly
 export default class App extends React.Component {
 
   state = {
-    mapHeight: 200
+    contentMarginTop: 200,
+    contentMarginTopAnim: new Animated.Value(100),
+    mapTopMargin: 0
   }
-
-
 
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <ScrollView style={{flex: 1}}>
+        <ScrollView style={{ flex: 1 }}
+        >
 
           <MapView
             onPress={() => {
               console.log('click');
-              this.setState({mapHeight: 300})
+              // this.setState({ contentMarginTop: 300 })
+
+              Animated.timing(
+                this.state.contentMarginTopAnim,
+                {
+                  toValue: 400,
+                  duration: 1000
+                }
+              ).start();
             }}
-            style={{ flex: 1, height: this.state.mapHeight }}
+            style={{
+              width: "100%",
+              height: 400,
+              position: "absolute",
+              left: 0,
+              top: 50,
+              zIndex: 1
+            }}
             initialRegion={{
               latitude: 37.78825,
               longitude: -122.4324,
@@ -29,8 +50,18 @@ export default class App extends React.Component {
             }}
 
           />
-          <Text style={{ height: 300 }}
-          >hello world</Text>
+          <Animated.View style={{
+            height: 300,
+            width: "100%",
+            position: "absolute",
+            left: 0,
+            top: this.state.contentMarginTopAnim,
+            zIndex: 2,
+            backgroundColor: "red"
+          }}>
+            <Text>hello world</Text>
+          </Animated.View>
+
         </ScrollView>
 
       </View>
